fb2024432eff1cd7e1c0854d59246fe1bbec8680
Andrey Antukh <niwi@niwi.be>
2013-03-23 18:42:37 +0100

    Now can create tasks in bulk mode.

--- app/js/controllers/dashboard.js
+++ app/js/controllers/dashboard.js
@@ -43,9 +43,44 @@ var DashboardController = function($scope, $rootScope, $routeParams, rs) {
 
     /* Global Scope Variables */
     $rootScope.pageSection = 'backlog';
+
+
+    $scope.newtaskForm = {};
+    $scope.createNewTask = function() {
+        var tasksText = $scope.newtaskForm.tasks;
+        var tasksUsId = parseInt($scope.newtaskForm.usId, 10);
+        $scope.newtaskForm = {};
+
+        var tasks = _.map(tasksText.split("\n"), function(text) {
+            var task = {id: _.uniqueId(), name: text, status_id:"new"};
+            var tokens = text.split(" ");
+
+            if (tokens[0][0] == "!") {
+                if (tokens[0] == "!inprogress") {
+                    task.status_id = "inprogress";
+                }
+                task.name = tokens.splice(1).join(" ");
+            }
+            return task;
+        });
+
+        _.each(tasks, function(task) {
+            $scope.usTasks[tasksUsId][task.status_id].push(task);
+        });
+
+        /* Notify to all modal directives
+         * for close all opened modals. */
+        $scope.$broadcast("close-modals");
+    };
 };
 
 DashboardController.$inject = ['$scope', '$rootScope', '$routeParams', 'resource'];
+
+var DashboardUserStoryController = function($scope, $q) {
+
+};
+
+DashboardUserStoryController.$inject = ['$scope', '$q'];
 
 
 var DashboardTaskController = function($scope, $q) {
--- app/js/directives/dashboard.js
+++ app/js/directives/dashboard.js
@@ -27,4 +27,27 @@ angular.module('greenmine.directives.dashboard', []).
                 scope.$digest();
             });
         };
+    }]).
+    directive("uiNewtaskModal", ["$parse", function($parse) {
+        return function(scope, elm, attrs) {
+            var element = angular.element(elm);
+            var modalElement = angular.element("#new-task-modal");
+
+            element.on("click", attrs.uiNewtaskModal, function(event) {
+                event.preventDefault();
+                var target = angular.element(event.currentTarget);
+
+                scope.newtaskForm.usId = target.scope().us.id;
+                modalElement.modal()
+            });
+
+            modalElement.on("click", ".buttons .cancel", function(event) {
+                event.preventDefault();
+                modalElement.modal('hide');
+            });
+
+            scope.$on('close-modals', function() {
+                modalElement.modal('hide');
+            });
+        };
     }]);
