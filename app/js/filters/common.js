ee474b86806eb28ea8e44321a4258b2a87f11630
Andrey Antukh <niwi@niwi.be>
2013-03-28 01:04:29 +0100

    Add truncate filter.

--- app/js/filters/common.js
+++ app/js/filters/common.js
@@ -6,6 +6,12 @@ angular.module('greenmine.filters.common', []).
             });
         };
     }]).
+    filter('truncate', function() {
+        return function(input, num) {
+            if (num === undefined) num =  25;
+            return _.str.prune(input, num);
+        };
+    }).
     filter('slugify', [function() {
         return function(input) {
             return _.str.slugify(input);
