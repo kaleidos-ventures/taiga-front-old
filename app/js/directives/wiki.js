c2f6438fa9d5c8eed45ef36539edac0dd37a0f10
Andrey Antukh <niwi@niwi.be>
2013-04-24 16:55:19 +0200

    Fix some wrong event handling with markitup.

--- app/js/directives/wiki.js
+++ app/js/directives/wiki.js
@@ -2,53 +2,60 @@
 
 angular.module('greenmine.directives.wiki', []).
     directive('gmMarkitup', ["$parse", function($parse) {
-        var markdownSettings = {
-            nameSpace:          'markdown', // Useful to prevent multi-instances CSS conflict
-            onShiftEnter:       {keepDefault:false, openWith:'\n\n'},
-            previewParser: function(content) { return markdown.toHTML(content); },
-            markupSet: [
-                {name:'First Level Heading', key:"1", placeHolder:'Your title here...', closeWith:function(markItUp) { return markdownTitle(markItUp, '=') } },
-                {name:'Second Level Heading', key:"2", placeHolder:'Your title here...', closeWith:function(markItUp) { return markdownTitle(markItUp, '-') } },
-                {name:'Heading 3', key:"3", openWith:'### ', placeHolder:'Your title here...' },
-                {name:'Heading 4', key:"4", openWith:'#### ', placeHolder:'Your title here...' },
-                {name:'Heading 5', key:"5", openWith:'##### ', placeHolder:'Your title here...' },
-                {name:'Heading 6', key:"6", openWith:'###### ', placeHolder:'Your title here...' },
-                {separator:'---------------' },
-                {name:'Bold', key:"B", openWith:'**', closeWith:'**'},
-                {name:'Italic', key:"I", openWith:'_', closeWith:'_'},
-                {separator:'---------------' },
-                {name:'Bulleted List', openWith:'- ' },
-                {name:'Numeric List', openWith:function(markItUp) {
-                    return markItUp.line+'. ';
-                }},
-                {separator:'---------------' },
-                {name:'Picture', key:"P", replaceWith:'![[![Alternative text]!]]([![Url:!:http://]!] "[![Title]!]")'},
-                {name:'Link', key:"L", openWith:'[', closeWith:']([![Url:!:http://]!] "[![Title]!]")', placeHolder:'Your text to link here...' },
-                {separator:'---------------'},
-                {name:'Quotes', openWith:'> '},
-                {name:'Code Block / Code', openWith:'(!(\t|!|`)!)', closeWith:'(!(`)!)'},
-                {separator:'---------------'},
-                {name:'Preview', call:'preview', className:"preview"}
-            ]
-        }
+        return {
+            require: "?ngModel",
+            link: function(scope, elm, attrs, ngModel) {
+                var markdownSettings = {
+                    nameSpace:          'markdown', // Useful to prevent multi-instances CSS conflict
+                    onShiftEnter:       {keepDefault:false, openWith:'\n\n'},
+                    previewParser: function(content) { return markdown.toHTML(content); },
+                    markupSet: [
+                        {name:'First Level Heading', key:"1", placeHolder:'Your title here...', closeWith:function(markItUp) { return markdownTitle(markItUp, '=') } },
+                        {name:'Second Level Heading', key:"2", placeHolder:'Your title here...', closeWith:function(markItUp) { return markdownTitle(markItUp, '-') } },
+                        {name:'Heading 3', key:"3", openWith:'### ', placeHolder:'Your title here...' },
+                        {name:'Heading 4', key:"4", openWith:'#### ', placeHolder:'Your title here...' },
+                        {name:'Heading 5', key:"5", openWith:'##### ', placeHolder:'Your title here...' },
+                        {name:'Heading 6', key:"6", openWith:'###### ', placeHolder:'Your title here...' },
+                        {separator:'---------------' },
+                        {name:'Bold', key:"B", openWith:'**', closeWith:'**'},
+                        {name:'Italic', key:"I", openWith:'_', closeWith:'_'},
+                        {separator:'---------------' },
+                        {name:'Bulleted List', openWith:'- ' },
+                        {name:'Numeric List', openWith:function(markItUp) {
+                            return markItUp.line+'. ';
+                        }},
+                        {separator:'---------------' },
+                        {name:'Picture', key:"P", replaceWith:'![[![Alternative text]!]]([![Url:!:http://]!] "[![Title]!]")'},
+                        {name:'Link', key:"L", openWith:'[', closeWith:']([![Url:!:http://]!] "[![Title]!]")', placeHolder:'Your text to link here...' },
+                        {separator:'---------------'},
+                        {name:'Quotes', openWith:'> '},
+                        {name:'Code Block / Code', openWith:'(!(\t|!|`)!)', closeWith:'(!(`)!)'},
+                        {separator:'---------------'},
+                        {name:'Preview', call:'preview', className:"preview"}
+                    ],
 
-        // mIu nameSpace to avoid conflict.
-        var markdownTitle = function(markItUp, char) {
-            var heading = '';
-            var n = $.trim(markItUp.selection||markItUp.placeHolder).length;
-            for(var i = 0; i < n; i++) {
-                heading += char;
+                    afterInsert: function(event) {
+                        var target = angular.element(event.textarea);
+                        ngModel.$setViewValue(target.val())
+                    }
+                }
+
+                // mIu nameSpace to avoid conflict.
+                var markdownTitle = function(markItUp, char) {
+                    var heading = '';
+                    var n = $.trim(markItUp.selection||markItUp.placeHolder).length;
+                    for(var i = 0; i < n; i++) {
+                        heading += char;
+                    }
+                    return '\n'+heading+'\n';
+                };
+
+                var element = angular.element(elm);
+                element.markItUp(markdownSettings);
+                element.on("keypress", function(event) {
+                    scope.$apply();
+                });
             }
-            return '\n'+heading+'\n';
-        }
-
-        return function(scope, elm, attrs) {
-            var element = angular.element(elm);
-            element.markItUp(markdownSettings);
-            element.on("keypress", function(event) {
-                scope.$apply();
-                //console.log(element.data());
-            });
         };
     }]).
     directive("gmRenderMarkdown", ["$parse", function($parse) {
